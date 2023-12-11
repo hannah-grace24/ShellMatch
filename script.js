282c478bc4feae50a562191ca9ff82b76cf0a243
hannah-grace24 <hannahgrace.morris24@gmail.com>
2023-12-11 14:13:45 -0600

    12/11

--- script.js
+++ script.js
@@ -3,8 +3,29 @@ const formOpenBtn = document.querySelector("#form-open"),
     formContainer = document.querySelector(".form-container"),
     formCloseBtn = document.querySelector(".form-close"),
     signupBtn = document.querySelector("#signup"),
-    signinBtn = document.querySelector("#signup"),
+    signinBtn = document.querySelector("#signin"),
     pwShowHide = document.querySelectorAll(".pw-hide");
 
-    formOpenBtn.addEventListener("click", () => home.classList.add("show"));
-    formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let getPwInput = icon.parentElement.querySelector("input");
+        if(getPwInput.type === "password") {
+            getPwInput.type = "text";
+            icon.classList.replace("ul-eye-slash", "uil-eye")
+        }
+    })
+})
+
+signupBtn.addEventListener("click", () =>  {
+    e.preventDefault();
+    formContainer.classList.add("active");
+
+});
+signinBtn.addEventListener("click", () =>  {
+    e.preventDefault();
+    formContainer.classList.remove("active");
+
+});
