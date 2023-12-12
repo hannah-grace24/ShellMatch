b4ef8ce77c9e751d7cd3ebdc71fea6ad1bc263b1
hannah-grace24 <hannahgrace.morris24@gmail.com>
2023-12-12 07:47:55 -0600

    12/12

--- script.js
+++ script.js
@@ -14,7 +14,10 @@ pwShowHide.forEach(icon => {
         let getPwInput = icon.parentElement.querySelector("input");
         if(getPwInput.type === "password") {
             getPwInput.type = "text";
-            icon.classList.replace("ul-eye-slash", "uil-eye")
+            icon.classList.replace("uil-eye-slash", "uil-eye")
+        }else{
+            getPwInput.type = "password";
+            icon.classList.replace("uil-eye", "uil-eye-slash")
         }
     })
 })
